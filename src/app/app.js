d81ae6858a33e8b4870b5fd06ae9177e964f050d
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-09-14 11:05:54 -0300

    Create routes

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,22 @@
 angular
     .module('github', [
         'ngRoute',
-        
+
         'github.search'
-    ]);
+    ])
+    .config(config);
+
+config.$inject = ['$routeProvider'];
+
+function config($routeProvider) {
+    $routeProvider
+        .when('/', {
+            template: '<h1>Search</h1>'
+        })
+        .when('/test/:user/', {
+            template: '<h1>User</h1>'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}
